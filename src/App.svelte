e1342f17cec7fd77434f5e5b2114c8298b181956
luddoro <luddenr1@live.se>
2021-04-29 13:50:53 +0200

    Can see if its wednesday

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { onMount } from 'svelte';
+	
+ 	let currentDate = new Date();
+
+// 	onMount(async () => {
+// 		const res = await fetch(`https://i.kym-cdn.com/entries/icons/original/000/020/016/wednesdaymydudeswide.jpg`);
+// 	 	frog = await res.json();
+// })
+
+
+ </script> 
+ 
+
+
+
+<div class="container">
+	<div class="content">
+		{#if currentDate.getDay() === 3}
+			<img src="/wednesdayfrog.png" alt="Wednesdayfroge">
+		{:else}
+			<h1> Its not my Wednesday my dudes :( </h1>
+		{/if}	 
+	</div> 
+</div>
+
+
+<style>
+	.container {
+		
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100vw;
+		margin: 0;
+		background-color: #dadada;
+	}
+	h1 {
+		color: #201f1f;
+		text-transform: uppercase;
+		font-size: 5em;
+		font-weight: 150;
+	}
+	/* main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	
+
+
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	} */
+</style>
+
+
---
+++ src/GoogleAPI.svelte
@@ -0,0 +1,28 @@
+<script>
+    function start() {
+      // 2. Initialize the JavaScript client library.
+      gapi.client.init({
+        'apiKey': 'AIzaSyAUrROeSqcJSfalzg-BVNC5vvf2ZsEagHw',
+      }).then(function() {
+        // 3. Initialize and make the API request.
+        return gapi.client.request({
+          'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+        })
+      }).then(function(response) {
+        console.log(response.result);
+      }, function(reason) {
+        console.log('Error: ' + reason.result.error.message);
+      });
+    };
+    // 1. Load the JavaScript client library.
+    gapi.load('client', start);
+    </script>
+
+<svelte:head> 
+    <!--Google core API-->
+    <script src="https://apis.google.com/js/api.js"></script>
+
+ 
+</svelte:head>
+
+
