d56b2c28657fe914d071ee2a9e81ab94a90fa3d6
ludde <luddenr1@live.se>
2021-05-08 22:11:35 +0200

    Realized this was an overcomplicated solution so just added youtube embed

--- src/App.svelte
+++ src/App.svelte
@@ -2,31 +2,59 @@
 	<title>Isitwednesdaymydudes?</title>
 </svelte:head>
 <script>
-
-import YoutubeAPI from './YoutubeAPI.svelte';
-	
  	let currentDate = new Date();
-
-
-
  </script> 
- 
-
-
 
 <div class="flex-wrapper">
 	<div class="content">
-		{#if currentDate.getDay() === 3}
-			<img class="frog" src="/wednesdayfrog.png" alt="Wednesdayfroge">
+		{#if currentDate.getDay() === 6}
+			<div id="frog-img">
+				<img class="frog" src="/wednesdayfrog.png" alt="Wednesdayfroge">
+			</div>
+			<div id="player"></div>
+			<script>
+				var tag = document.createElement('script');
+
+				tag.src = "https://www.youtube.com/iframe_api";
+				var firstScriptTag = document.getElementsByTagName('script')[0];
+				firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+				var now = new Date();
+				var start = new Date(now.getFullYear(), 0, 0);
+				var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+				var oneDay = 1000 * 60 * 60 * 24;
+				var day = Math.floor(diff / oneDay);
+
+	
+			index = Math.floor((day % 53) + 1);
+			var player;
+			function onYouTubeIframeAPIReady() {
+				player = new YT.Player('player', {
+					height: '390',
+					width: '640',
+					playerVars: {
+								listType:'playlist',
+								list:'PLy3-VH7qrUZ5IVq_lISnoccVIYZCMvi-8',
+								index: index,
+					},
+					events: {
+						'onReady': function (event) {
+					
+							event.target.playVideo();
+							setTimeout(function() {
+								event.target.setShuffle({'shufflePlaylist' : true});
+							}, 1000);
+						}
+					}
+				});
+			}
+			</script>
 		{:else}
 			<h1> Its not Wednesday my dudes :( </h1>
 		{/if}	 
 	</div> 
-	<div class="youtubePlayer">
-		<YoutubeAPI></YoutubeAPI>
-	</div>
-</div>
 
+</div>
 
 <style>
 
@@ -45,12 +73,19 @@ import YoutubeAPI from './YoutubeAPI.svelte';
 	font-weight: 10;
 	overflow: hidden;
 }
+
 .frog {
 	width: 100%;
 	height: auto;
 	max-width: 400px;
 }
 
+#player {
+	max-width: 100%
+}
+
+#frog-img {
+	width: 100%; 
+	text-align: center;
+}
 </style>
-
-
